a892b5903733c52825d6baff7483dc78b8ad8573
Leonidas Esteban <leonidasesteban@gmail.com>
2018-05-29 16:04:20 -0500

    clase 8 - estilos

--- App.js
+++ App.js
@@ -48,7 +48,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: Platform.select({
+      ios: 'blue',
+      android: 'purple',
+    }),
   },
   welcome: {
     fontSize: 20,
