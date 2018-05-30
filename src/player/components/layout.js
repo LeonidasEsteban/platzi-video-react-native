ff992bc380291865bc8953e45b588c2b2332dc5e
Leonidas Esteban <leonidasesteban@gmail.com>
2018-05-30 17:44:19 -0500

    clase 22 - controlando el buffer de video

--- src/player/components/layout.js
+++ src/player/components/layout.js
@@ -11,6 +11,12 @@ function Layout(props) {
       <View style={styles.video}>
         {props.video}
       </View>
+      <View style={styles.overlay}>
+        {
+          props.loading &&
+          props.loader
+        }
+      </View>
     </View>
   )
 }
@@ -26,6 +32,15 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
     backgroundColor: 'black',
+  },
+  overlay: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
 
--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import Video from 'react-native-video';
 import {
-  StyleSheet
+  StyleSheet,
+  ActivityIndicator
 } from 'react-native';
 import Layout from '../components/layout';
 
 class Player extends Component {
+  state = {
+    loading: true,
+  }
+  onBuffer = ({ isBuffering }) => {
+    this.setState({
+      loading: isBuffering
+    })
+  }
+  onLoad = () => {
+    this.setState({
+      loading: false
+    })
+  }
   render() {
     return (
       <Layout
+        loading={this.state.loading}
         video={
           <Video
             source={{uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
             style={styles.video}
             resizeMode="contain"
+            onBuffer={this.onBuffer}
+            onLoad={this.onLoad}
           />
+        }
+        loader={
+          <ActivityIndicator color="red" />
         }
       />
     )
