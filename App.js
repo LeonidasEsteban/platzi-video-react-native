f2823addbc75bf559727c3ab01cfc98af0192a69
Leonidas Esteban <leonidasesteban@gmail.com>
2018-05-30 16:46:25 -0500

    clase 20 - instalando plugins

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   Text,
+  View,
 } from 'react-native';
 
 import Home from './src/screens/containers/home';
@@ -8,6 +9,7 @@ import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
 import API from './utils/api';
 import CategoryList from './src/videos/containers/category-list.js';
+import Video from 'react-native-video';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -29,6 +31,24 @@ export default class App extends Component<Props> {
     return (
       <Home>
         <Header />
+        <View
+          style={{
+            flex: 1,
+            height: 100,
+          }}
+        >
+          <Video
+            source={{uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+            }}
+            resizeMode="contain"
+          />
+        </View>
         <Text>buscador</Text>
         <Text>categorías</Text>
         <CategoryList
