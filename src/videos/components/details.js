734fb522eb5e750dfa5f53f7c83ea969c8a1dc7a
Leonidas Esteban <leonidasesteban@gmail.com>
2018-05-31 18:11:59 -0500

    clase 32 - WebView y ScrollView

--- src/videos/components/details.js
+++ src/videos/components/details.js
@@ -3,12 +3,39 @@ import {
   View,
   Text,
   Image,
-  StyleSheet
+  StyleSheet,
+  WebView,
+  ScrollView,
 } from 'react-native';
+
+
+
+const makeHTML = (id) => {
+  return (`
+    <style>
+      .video {
+        position: relative;
+        padding-bottom: 56.25%;
+      }
+      iframe {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+      }
+    </style>
+    <div class="video">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+  `)
+}
 
 function Details(props) {
   return (
-    <View>
+    <ScrollView>
       <View style={styles.top}>
         <Text>{props.title}</Text>
       </View>
@@ -20,9 +47,13 @@ function Details(props) {
           />
           <Text style={styles.description}>{props.description_full}</Text>
         </View>
-
       </View>
-    </View>
+      <View style={styles.trailer}>
+        <WebView
+          source={{html: makeHTML(props.yt_trailer_code ) }}
+        />
+      </View>
+    </ScrollView>
   )
 }
 
@@ -31,6 +62,7 @@ const styles = StyleSheet.create({
   },
   trailer: {
     height: 200,
+    marginBottom: 10,
   },
   details: {
     flexDirection: 'row',
