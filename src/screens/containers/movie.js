37af3ba540e09efcdcf881bd8a6fdc218e0ab07e
Leonidas Esteban <leonidasesteban@gmail.com>
2018-05-31 19:14:04 -0500

    clase 35 - animaciones

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -5,8 +5,14 @@ import Header from '../../sections/components/header';
 import Close from '../../sections/components/close';
 import { connect } from 'react-redux';
 import Details from '../../videos/components/details';
+import {
+  Animated
+} from 'react-native';
 
 class Movie extends Component {
+  state = {
+    opacity: new Animated.Value(0),
+  }
   closeVideo = () => {
     this.props.dispatch({
       type: 'SET_SELECTED_MOVIE',
@@ -15,17 +21,33 @@ class Movie extends Component {
       }
     })
   }
+  componentDidMount() {
+    Animated.timing(
+      this.state.opacity,
+      {
+        toValue: 1,
+        duration: 1000,
+      }
+    ).start();
+  }
   render() {
     return (
-      <MovieLayout>
-        <Header>
-          <Close
-            onPress={this.closeVideo}
-          />
-        </Header>
-        <Player />
-        <Details {...this.props.movie}/>
-      </MovieLayout>
+      <Animated.View
+        style={{
+          flex: 1,
+          opacity: this.state.opacity,
+        }}
+      >
+        <MovieLayout>
+          <Header>
+            <Close
+              onPress={this.closeVideo}
+            />
+          </Header>
+          <Player />
+          <Details {...this.props.movie}/>
+        </MovieLayout>
+      </Animated.View>
     )
   }
 }
