439d44c5f1cb27389e2d96d2683a90c98ca7fe41
Leonidas Esteban <leonidasesteban@gmail.com>
2018-06-01 17:25:47 -0500

    clase 36 - build de producción android

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ import Search from './sections/containers/search';
 class AppLayout extends Component {
   async componentDidMount() {
     const categoryList = await API.getMovies();
+    console.log('Esta es la lista de categorías')
+    console.table(categoryList);
     this.props.dispatch({
       type: 'SET_CATEGORY_LIST',
       payload: {
